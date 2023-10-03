dcab5226f8cf4c4598f0a3ea37742dd4df9ef753
Luciano F. Gonçalves <luciano.fg.dv@gmail.com>
2023-10-03 18:51:22 -0300

    Fixing indentation in some files

--- lua/lightdarkmodeconfig.lua
+++ lua/lightdarkmodeconfig.lua
@@ -1,16 +1,12 @@
 -- Toggle Light/Dark mode
 function LightDarkModeToggle()
-  if vim.o.background == 'light' then
-    vim.cmd('set background=dark')
-    os.execute([[
-       echo 'vim.o.background = "dark"' > $HOME/.config/nvim/lua/light.lua
-    ]])
-  else
-    vim.cmd('set background=light')
-    os.execute([[
-       echo 'vim.o.background = "light"' > $HOME/.config/nvim/lua/light.lua
-   ]])
-  end
+    if vim.o.background == 'light' then
+        vim.cmd('set background=dark')
+        os.execute([[echo 'vim.o.background = "dark"' > $HOME/.config/nvim/lua/light.lua]])
+    else
+        vim.cmd('set background=light')
+        os.execute([[echo 'vim.o.background = "light"' > $HOME/.config/nvim/lua/light.lua]])
+    end
 end
 
 vim.keymap.set('n', '<M-l>', ':lua LightDarkModeToggle()<CR>', { noremap = true, silent = true })
